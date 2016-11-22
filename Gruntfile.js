dcc707fe7c25e33edafe5056d21f179a77788249
Vassilis Krikonis <vasiliskrikonis@gmail.com>
2016-11-23 01:05:43 +0200

    previous gruntfile used 100% cpu

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,74 +1,22 @@
-// Gruntfile with the configuration of grunt-express and grunt-open. No livereload yet!
 module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-express');
 
-  // Load Grunt tasks declared in the package.json file
-  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-
-  // Configure Grunt
   grunt.initConfig({
-
-    // grunt-express will serve the files from the folders listed in `bases`
-    // on specified `port` and `hostname`
-    express: {
-      all: {
-        options: {
-          port: 9000,
-          hostname: "0.0.0.0",
-          bases: [__dirname], // Replace with the directory you want the files served from
-                              // Make sure you don't use `.` or `..` in the path as Express
-                              // is likely to return 403 Forbidden responses if you do
-                              // http://stackoverflow.com/questions/14594121/express-res-sendfile-throwing-forbidden-error
-          livereload: true
-        }
-      }
-    },
-
-    // grunt-watch will monitor the projects files
     watch: {
-      all: {
-        // Replace with whatever file you want to trigger the update from
-        // Either as a String for a single entry
-        // or an Array of String for multiple entries
-        // You can use globing patterns like `css/**/*.css`
-        // See https://github.com/gruntjs/grunt-contrib-watch#files
-        files: ['index.html', 'styles.css', 'script.js'],
+      files: ['./*.html', './*.js', './*.css'],
+      options: {
+        livereload: true
+      }
+    },
+    express: {
+      server: {
         options: {
-          livereload: true
-        }
-      }
-    },
-
-    // grunt-open will open your browser at the project's URL
-    open: {
-      all: {
-        // Gets the port from the connect configuration
-        path: 'http://localhost:<%= express.all.options.port%>'
-      }
-    },
-
-    // bower_concat will concatenate bower files
-    bower_concat: {
-      all: {
-        dest: 'all_bower.js',
-        mainFiles: {
-          'poly-decomp': ['build/decomp.js']
-        },
-        callback: function(mainFiles, component) {
-          return mainFiles.map(function(filepath) {
-            // Use minified files if available
-            var min = filepath.replace(/\.js$/, '.min.js');
-            return grunt.file.exists(min) ? min : filepath;
-          });
+          bases: ['.']
         }
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-bower-concat');
-
-  // Creates the `server` task
-  grunt.registerTask('server', [
-    'express',
-    'watch'
-  ]);
+  grunt.registerTask('serve', ['express', 'watch']);
 };
