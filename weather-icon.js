73bd7b0ba05db2bf6ca01c1c2c05a5d689bf4ebf
Vassilis Krikonis <vasiliskrikonis@gmail.com>
2016-11-24 01:56:53 +0200

    icon is a custom element

---
+++ weather-icon.js
@@ -0,0 +1,60 @@
+class WeatherIcon extends HTMLElement {
+	constructor() {
+		super();
+
+		this.folderPath = 'icons/set2/';
+		this.icons = {
+			clearDay: 'sun.svg',
+			clearNight: 'moon.svg',
+			rain: 'rain.svg',
+			snow: '',
+			sleet: '',
+			wind: 'wind.jpg',
+			fog: 'fog-lighthouse1.svg',
+			cloudy: 'clouds.svg',
+			partlyCloudyDay: 'partly-day.svg',
+			partlyCloudyNight: 'partly-night.svg'
+		};
+	}
+
+	static get observedAttributes() {
+		return ['icon'];
+	}
+
+	connectedCallback() {
+		let img = new Image();
+		img.src = this.folderPath + this.icon.filename;
+		img.setAttribute('alt', this.icon.name);
+		img.setAttribute('title', this.icon.name);
+		img.onload = this._onIconLoad.bind(this);
+		this.appendChild(img);
+	}
+
+	disconnectedCallback() {
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		if(name === 'icon') {
+			let icon = this._kebabToCamelCase(newValue);
+			if(this.icons[icon]) {
+				this.icon = {
+					name: icon,
+					value: newValue,
+					filename: this.icons[icon]
+				};
+			} else {
+				console.log('not a valid attribute value');
+			}
+		}
+	}
+
+	_onIconLoad() {
+		this.dispatchEvent(new Event('load'));
+	}
+
+	_kebabToCamelCase(str) {
+		return str.replace(/-([a-z])/g, (match, p1) => p1.toUpperCase());
+	}
+}
+
+window.customElements.define('weather-icon', WeatherIcon);
