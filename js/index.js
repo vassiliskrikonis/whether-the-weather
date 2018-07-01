0260194174fb75ede4e197a8975ad839885c9c7e
Vassilis Krikonis <vasiliskrikonis@gmail.com>
2018-07-01 20:53:45 +0300

    Add geolocation

--- js/index.js
+++ js/index.js
@@ -1,6 +1,23 @@
-const text = `
+const KEY = 'ac85f65c639c545e0acc46a5f678d9fd';
+
+const infoHtml = `
   <div class="when">Today</div>
   <div>it's <span id="what">hotter</span><sup>1</sup></div>
   <div>than</div>
   <div class="when">yesterday</div>
 `;
+
+try {
+  navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
+}
+catch(err) {
+  document.body.innerHTML = '<p style="text-align:center;">Geolocation is not supported</p>';
+}
+
+function onGeoSuccess(pos) {
+  const {latitude, longitude} = pos.coords;
+}
+
+function onGeoError(err) {
+  console.warn(err);
+}
