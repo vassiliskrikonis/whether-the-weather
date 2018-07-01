837b83fa2097c08dc7dae0f96f44ebe735c83210
Vassilis Krikonis <vasiliskrikonis@gmail.com>
2018-07-01 20:15:16 +0300

    Initial loading state

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # whether-the-weather
 a simple web app about the weather
+
+## Credits
+
+- hourglass by Alina Oleynik from the Noun Project
---
+++ js/index.js
@@ -0,0 +1,6 @@
+const text = `
+  <div class="when">Today</div>
+  <div>it's <span id="what">hotter</span><sup>1</sup></div>
+  <div>than</div>
+  <div class="when">yesterday</div>
+`;
