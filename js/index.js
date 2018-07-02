d54f4451173c0616f80aa306bc336aa496598161
Vassilis Krikonis <vasiliskrikonis@gmail.com>
2018-07-02 21:29:08 +0300

    Use proxy for darksky to get the two days weather data.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,4 @@
-const KEY = 'ac85f65c639c545e0acc46a5f678d9fd';
-
-const infoHtml = `
-  <div class="when">Today</div>
-  <div>it's <span id="what">hotter</span><sup>1</sup></div>
-  <div>than</div>
-  <div class="when">yesterday</div>
-`;
+import axios from 'axios';
 
 try {
   navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
@@ -16,6 +9,33 @@ catch(err) {
 
 function onGeoSuccess(pos) {
   const {latitude, longitude} = pos.coords;
+  console.log(`Received geolocation: lat: ${latitude}, lon: ${longitude}`);
+
+  const options = {
+    latitude,
+    longitude,
+    time: (new Date()).toISOString()
+  }
+
+  axios.post('https://weather-the-weather-proxy.glitch.me/', options)
+  .then(response => {
+    const [today, yesterday] = response.data;
+    const todaysTemp = today.currently.apparentTemperature;
+    const icon = today.currently.icon;
+    const yesterdaysTemp = yesterday.currently.apparentTemperature;
+
+    console.log(`Received weather data: todays temp is ${todaysTemp}, yesterdays temp is ${yesterdaysTemp}`);
+
+    const infoHtml = `
+      <div class="when">Today</div>
+      <div>it's <span id="what">${todaysTemp >= yesterdaysTemp ? 'hotter' : 'colder'}</span><sup>1</sup></div>
+      <div>than</div>
+      <div class="when">yesterday</div>
+    `;
+
+    document.querySelector('.info').innerHTML = infoHtml;
+  })
+  .catch(console.warn)
 }
 
 function onGeoError(err) {
