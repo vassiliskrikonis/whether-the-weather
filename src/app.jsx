e67178e5b48dc48caff22cf49205551568865fe5
Vassis Krikonis <vassiliskrikonis@gmail.com>
2019-12-26 20:32:55 +0200

    Fade in-out  icon & info on changeUsing react-animation library

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import ReactDom from "react-dom";
 import "../css/styles.css";
 import Footer from "./footer";
@@ -7,6 +7,7 @@ import { DateTime } from "luxon";
 import axios from "axios";
 import Icon from "./icon";
 import Info from "./info";
+import { AnimateOnChange } from "react-animation";
 
 function useGeoLocation() {
   const [location, setLocation] = useState(null);
@@ -25,8 +26,10 @@ function useGeoLocation() {
 }
 
 function useDarkSky(location) {
-  const [todaysTemp, setTodaysTemp] = useState(null);
-  const [yesterdaysTemp, setYesterdaysTemp] = useState(null);
+  const [temperatures, setTemperatures] = useState({
+    today: null,
+    yesterday: null
+  });
   const [icon, setIcon] = useState(null);
 
   useEffect(() => {
@@ -42,25 +45,35 @@ function useDarkSky(location) {
       const icon = today.currently.icon;
       const yesterdaysTemp = yesterday.currently.apparentTemperature;
 
-      setTodaysTemp(todaysTemp);
-      setYesterdaysTemp(yesterdaysTemp);
+      setTemperatures({
+        today: todaysTemp,
+        yesterday: yesterdaysTemp
+      });
       setIcon(icon);
     });
   }, [location]);
 
-  return { todaysTemp, yesterdaysTemp, icon };
+  return { temperatures, icon };
 }
 
 const App = () => {
   const location = useGeoLocation();
-  const { icon, todaysTemp, yesterdaysTemp } = useDarkSky(location);
+  const {
+    icon,
+    temperatures: { today: todaysTemp, yesterday: yesterdaysTemp }
+  } = useDarkSky(location);
   const loaded = [icon, todaysTemp, yesterdaysTemp].every(v => v !== null);
+
+  const renderedIcon = useMemo(() => <Icon key={icon} icon={icon || "loading"} />, [icon]);
+  const renderedInfo = useMemo(() => {
+    return loaded ? <Info today={todaysTemp} yesterday={yesterdaysTemp} /> : <p>Loading...</p>;
+  }, [loaded]);
 
   return (
     <div className="weather-app">
       <div className="weather-wrapper">
-        <Icon icon={icon || "loading"} />
-        {loaded ? <Info today={todaysTemp} yesterday={yesterdaysTemp} /> : <p>Loading...</p>}
+        <AnimateOnChange>{renderedIcon}</AnimateOnChange>
+        <AnimateOnChange>{renderedInfo}</AnimateOnChange>
       </div>
       <Footer />
     </div>
